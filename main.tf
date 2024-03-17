4f869815be459a454bf0d7e3229af5c9015f9f4e
aline.tanure <alinedonatoc@gmail.com>
2024-03-17 10:42:16 -0300

    Add vpc tag

--- main.tf
+++ main.tf
@@ -29,6 +29,10 @@ module "vpc" {
 
   name = "education-vpc"
 
+  tags = {
+    Name = "postech-vpc"
+  }
+
   cidr = "10.0.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
